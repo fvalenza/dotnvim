5702dcbffd94f5705e836d2477d0da25712754d0
Florian Valenza <florian.valenza@gmail.com>
2023-07-14 01:29:23 +0200

    Adding aerial for symbols outline. To compare with Telescope lsp_document_symbols. ( or perhaps use both ? )

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -81,3 +81,6 @@ keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
 
 -- Lsp
 keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+
+-- Aerial
+vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle<CR>')

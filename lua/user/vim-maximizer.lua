20ab72ace2eaa2565bc8f83088df4834baee0162
Florian Valenza <florian.valenza@gmail.com>
2023-07-14 17:02:27 +0200

    Added vim-maximizer

---
+++ lua/user/vim-maximizer.lua
@@ -0,0 +1,9 @@
+local M = {
+  "szw/vim-maximizer",
+  -- commit = "f5d970d4506f385b29534252d8c15a782fa53034",
+  keys = {
+    { "<leader>sm", '<cmd>MaximizerToggle<CR>', desc = "Toggle split Maximization", mode = "n", opts_keymap },
+  },
+}
+
+return M

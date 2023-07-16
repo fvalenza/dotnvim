d42786207088259dfd86e4c14631af08be43df77
Florian Valenza <florian.valenza@gmail.com>
2023-07-17 00:15:21 +0200

    Fixed config of nvim-surround

--- lua/user/nvim-surround.lua
+++ lua/user/nvim-surround.lua
@@ -1,19 +1,15 @@
--- not working ???
 local opts_keymap = { silent = true }
 
 local M = {
   "kylechui/nvim-surround",
-  -- keys = {
-  --   { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-  --   { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-  --   { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-  --   { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-  --   { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  -- },
+  version = "*",
+  event = "VeryLazy",
 }
 
 function M.config()
-  require("nvim-surround").setup()
+  require("nvim-surround").setup({
+
+  })
 end
 
 return M
